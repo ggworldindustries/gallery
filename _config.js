c6c4ed1271f120e0130d7293926541c9289ab265
Gideon Gachie <Gachiegideon@outlook.com>
2024-09-22 21:36:28 +0300

    Update MongoDB connection strings in _config.js

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://gideongachie:VXz7grirhkmLoR3O@gallery.nn9xf.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
-    development: 'mongodb+srv://gideongachie:VXz7grirhkmLoR3O@gallery.nn9xf.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
-    test: 'mongodb+srv://gideongachie:VXz7grirhkmLoR3O@gallery.nn9xf.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    production: 'mongodb+srv://gideongachie:33813398@gallery.nn9xf.mongodb.net/darkroom?retryWrites=true&w=majority&appName=gallery',
+    development: 'mongodb+srv://gideongachie:33813398@gallery.nn9xf.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=gallery',
+    test: 'mongodb+srv://gideongachie:33813398@gallery.nn9xf.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=gallery',
 }
 module.exports = config;
  
